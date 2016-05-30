590e1178a86b49c5983607e74499f7afaa85542c
黄黄波 <848032671@qq.com>
2016-05-30 09:45:03 +0800

    Initial Commit

---
+++ 小师妹/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  小师妹
+//
+//  Created by 黄黄波 on 16/5/30.
+//  Copyright © 2016年 junior-sister. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 小师妹/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  小师妹
+//
+//  Created by 黄黄波 on 16/5/30.
+//  Copyright © 2016年 junior-sister. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
